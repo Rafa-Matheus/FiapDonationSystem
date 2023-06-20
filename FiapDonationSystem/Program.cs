2896002d0b6b1a9d67cd75ba04a323d73dbd6237
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-06-20 17:38:02 -0300

    Start of project development

---
+++ FiapDonationSystem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiapDonationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("FIAP Donation System!");
+			//char letra = 'e';
+			Console.WriteLine("Digite seu nome: ");
+			string nome = Console.ReadLine();
+			Console.WriteLine("Digite seu telefone: ");
+			string telefone = Console.ReadLine();
+			Console.WriteLine("Digite seu email: ");
+			string email = Console.ReadLine();
+			Console.WriteLine("Digite sua senha: ");
+			string senha = Console.ReadLine();
+			Console.WriteLine("Digite seu apelido: ");
+			string apelido = Console.ReadLine();
+		}
+    }
+}
