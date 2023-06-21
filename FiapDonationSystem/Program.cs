9731506091d294dd7452e4a88783d9712641a6db
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-06-21 10:32:32 -0300

    Updated the conditional structure of Program.cs

--- FiapDonationSystem/Program.cs
+++ FiapDonationSystem/Program.cs
@@ -10,18 +10,51 @@ namespace FiapDonationSystem
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("FIAP Donation System!");
-			//char letra = 'e';
-			Console.WriteLine("Digite seu nome: ");
-			string nome = Console.ReadLine();
-			Console.WriteLine("Digite seu telefone: ");
-			string telefone = Console.ReadLine();
-			Console.WriteLine("Digite seu email: ");
-			string email = Console.ReadLine();
-			Console.WriteLine("Digite sua senha: ");
-			string senha = Console.ReadLine();
-			Console.WriteLine("Digite seu apelido: ");
-			string apelido = Console.ReadLine();
+			string nome, telefone, email, senha, apelido;
+
+			Console.WriteLine(" -- FIAP Donation System -- ");
+
+			Console.WriteLine("MENU:");
+			Console.WriteLine("1 - Registrar um novo usuário\n2 - Logar\n3 - Sair");
+			Console.Write("\nDigite a opção desejada: ");
+			int opcao = Convert.ToInt32(Console.ReadLine());
+
+			if (opcao == 1)
+			{
+				Console.Clear();
+				Console.WriteLine("FIAP Donation System");
+				Console.WriteLine("Digite seu nome: ");
+				nome = Console.ReadLine();
+				Console.WriteLine("Digite seu telefone: ");
+				telefone = Console.ReadLine();
+				Console.WriteLine("Digite seu email: ");
+				email = Console.ReadLine();
+				Console.WriteLine("Digite sua senha: ");
+				senha = Console.ReadLine();
+				Console.WriteLine("Digite seu apelido: ");
+				apelido = Console.ReadLine();
+			}
+			else if(opcao == 2)
+            {
+				Console.WriteLine("Digite o seu email: ");
+				string emailUsuario = Console.ReadLine();
+				Console.WriteLine("Digite a sua senha: ");
+				string senhaUsuario = Console.ReadLine();
+
+				if (emailUsuario.Equals("admin123@email.com"))
+				{
+					if (senhaUsuario.Equals("admin123"))
+						Console.WriteLine("Bem-vindo ao sistema " + emailUsuario);
+					else
+						Console.WriteLine("Usuário e/ou Senha inválidos");
+				}
+				else
+					Console.WriteLine("Usuario e/ou senha inválidos");
+            }
+			else
+                Console.WriteLine("Obrigado por usar o sistema");
+
+			Console.ReadKey();
 		}
     }
 }
